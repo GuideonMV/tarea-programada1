0a070b721ca1efaed93f51f21a3cd7784cad4a8c
GuideonMV <guideon.montero65@gmail.com>
2026-04-24 23:18:06 -0600

    Estructura inicial del menú

---
+++ main.py
@@ -0,0 +1,30 @@
+
+def menu():
+    while True:
+        print("===========MENÚ PRINCIPAL===========")
+        print("1. Cargar tokens\n2. Mostrar tokens\n3. Agregar/modificar token\n4. Guardar tokens\n5. Traducir código\n6. Generar CSV\n7. Generar HTML\n8. Bitácora (submenú)\n9. Salir")
+        opcion = int(input("Seleccione una opción: "))
+        if opcion == 1:
+            pass
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            pass
+        elif opcion == 6:
+            pass
+        elif opcion == 7:
+            pass
+        elif opcion == 8:
+            pass
+        elif opcion == 9:
+            print("Saliendo del programa")
+            break
+        else:
+            print("Opción Inválida")
+
+#PP
+menu()
