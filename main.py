f29fa071aba482af0b3fe343cbe38e64e4a925f8
jimen4cuna <gimena.acuna@gmail.com>
2026-04-27 14:33:32 -0600

    Se agregó la opción 3 del menú

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#
 #Elaborado por: Jimena Acuña Parra y Guideon Montero Vargas
 #Fecha de elaboración: 18/04/2026 8:30pm
 #Última fecha de modificación: 20/04/2026 10:08pm
@@ -15,28 +14,27 @@ def menu():
     while True:
         print("===========MENÚ PRINCIPAL===========")
         print("1. Cargar tokens\n2. Mostrar tokens\n3. Agregar/modificar token\n4. Guardar tokens\n5. Traducir código\n6. Generar CSV\n7. Generar HTML\n8. Bitácora (submenú)\n9. Salir")
-        opcion = int(input("Seleccione una opción: "))
-        if opcion == 1:
+        opcion = (input("Seleccione una opción: "))
+        if opcion == "1":
             print(tokens.cargarTokensAux(listaTokens))
-        elif opcion == 2:
+        elif opcion == "2":
             tokens.mostrarTokens(listaTokens)
-        elif opcion == 3:
+        elif opcion == "3":
+            print(tokens.agregarModif(listaTokens))
+        elif opcion == "4":
             pass
-        elif opcion == 4:
+        elif opcion == "5":
             pass
-        elif opcion == 5:
+        elif opcion == "6":
             pass
-        elif opcion == 6:
+        elif opcion == "7":
             pass
-        elif opcion == 7:
+        elif opcion == "8":
             pass
-        elif opcion == 8:
-            pass
-        elif opcion == 9:
+        elif opcion == "9":
             print("Saliendo del programa")
             break
         else:
             print("Opción Inválida")
-
 #PP
 menu()
--- tokens.py
+++ tokens.py
@@ -3,6 +3,7 @@ def estarVacio(pArchivo):
         pArchivo.seek(0)
         return False
     return True
+
 def cargarTokensAux(tokens):
     while True:
         nombreArchivo = input("Ingrese el nombre del archivo: ")
@@ -27,9 +28,11 @@ def cargarTokensAux(tokens):
             return cargarTokens()
         except:
             print("El separador ingresado no es correcto")
+
 def cargarTokens():
         print("====================================")
         return "¡Tokens agregados correctamente!"
+
 def mostrarTokens(listaTokens):
     if len(listaTokens) == 0:
         print("No hay tokens cargados")
@@ -37,4 +40,46 @@ def mostrarTokens(listaTokens):
     print("\n===========TOKENS CARGADOS===========")
     for tupla in listaTokens:
         print(tupla[0], "->", tupla[1])
-   
+
+def agregarModif(pListaTokens):
+    textoTokens= input("Ingrese texto a procesar: ")
+    separador = input("Ingrese el sepador utilizado en el texto: ")
+    print("====================================")
+    tokens = textoTokens.split()
+    for elemento in tokens:
+        elemento = elemento.strip()
+        partes = elemento.split(separador)
+        palabraReservada = partes[0]
+        traduccion = partes[1]
+        if existeToken(pListaTokens, palabraReservada):
+            print(f"¡El token '{palabraReservada + " " + separador + " " + traduccion}' se encontró en la base de datos!\nSe procederá a modificarlo")
+            print("====================================")
+            print(modificarToken(pListaTokens, palabraReservada))
+            print("====================================")
+        else:
+            print(f"No se encontró el token '{palabraReservada + " " + separador + " " + traduccion}' en la base de datos\nSe procederá a agregarlo")
+            print("====================================")
+            print(agregarToken(pListaTokens, palabraReservada, traduccion))
+    print("====================================")
+    return "Proceso finalizado :)"
+
+def existeToken(pListaTokens, pToken):
+    for tupla in pListaTokens:
+        if pToken == tupla[0]:
+            return True
+    return False
+
+def modificarToken(pListaTokens, pPalabraReservada):
+        for i in range(len(pListaTokens)):
+            tupla = pListaTokens[i]
+            palabraReservada = tupla[0]
+            if palabraReservada == pPalabraReservada:
+                nuevoReemplazo = input("Ingrese el nuevo reemplazo: ")
+                pListaTokens[i] = (pPalabraReservada, nuevoReemplazo)
+                print("====================================")
+        return "¡El token ha sido modificado correctamente!"
+               
+def agregarToken(pListaTokens, pPalabraReservada, pTraduccion):
+    nuevoToken = [pPalabraReservada, pTraduccion]
+    pListaTokens.append(nuevoToken)
+    return "¡El token se agregó correctamente!"
